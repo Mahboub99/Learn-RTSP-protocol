7e6ef8fde01af2b2e90b6b4cc0a9c8162a54de0c
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2020-06-22 16:40:00 +0200

    add pause and play buttons

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ stream = new Stream({
   name: "name",
 
   streamUrl: "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_175k.mov",
-  wsPort: 9999
+  wsPort: 9999,
+  ffmpegOptions: {
+    // options ffmpeg flags
+    "-stats": "", // an option with no neccessary value uses a blank string
+    "-r": 100 // options with required values specify the value after the key
+  }
 });
